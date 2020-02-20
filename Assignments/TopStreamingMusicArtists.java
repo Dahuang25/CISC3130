97b2653d423de921511aaaf50706d6feb7bb2ae4
Dahuang25 <60986982+Dahuang25@users.noreply.github.com>
2020-02-20 15:56:13 -0500

    Update and rename Top Streaming Music Artists to TopStreamingMusicArtists.java

--- Assignments/TopStreamingMusicArtists.java
+++ Assignments/TopStreamingMusicArtists.java
@@ -1,18 +1,32 @@
+/*
+  Author: Da Huang
+  Class: CISC 3130(TY2) - Data Structures
+  Instructor: Katherine Chuang
+  Due Date: 2/20/2020
+*/
+
+/*
+  Assignment: Top Streaming Music Artists
+  The goal of the assignment is to read data from the file
+  Sort the artists name in alphabetical order
+  Count how many time the music artists appeared in the list
+*/
+
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class TopStreamingMusicArtists {
   public static void main(String[] args) throws Exception {
     // create Scanner to open up file
     Scanner scan = new Scanner(new File("regional-global-weekly-2020-01-17--2020-01-24.csv"));
     int col = 5;
     ArrayList<Artist> artists = new ArrayList<>();
-    // read file using Scan
+    // read file using scanner
     // read if there are more lines available
     while(scan.hasNextLine()) {
       boolean found = false;
       String line = scan.nextLine();
-      String [] arr = line.split(",");
+      String [] arr = line.split(",");  //using string.split to split the comma in the file 
       // checking for duplicates
       for(int x = 0; x <artists.size(); x++) {
         artists.get(x);
@@ -25,6 +39,8 @@ public class Main {
             break;
         }
       }
+      // if artist is not in the arraylist then it will add the artists name to the list 
+      // it will also remove the quotation mark around the artists name
       if(!found) {
         Artist a = new Artist(arr[2].replace("\"", ""), 1);
         artists.add(a);
@@ -41,7 +57,7 @@ public class Main {
         }
       }
     }
-
+    // print out the artists name and count how many times the artists name appear in the file
     for(Artist a : artists) {
       System.out.println(a.getName() + " " + a.getTrack());
     }
@@ -51,26 +67,32 @@ public class Main {
 // create a class Artist to set and get Artist name and count them
 class Artist {
 
+  // set name and trackNo as private
   private String name;
   private int trackNo;
-
+  
+  // create constructor
   public Artist(String n, int t) {
     name = n;
     trackNo = t;
   }
   
+  // create accessor to access the private variable name
   public String getName() {
     return name;
   }
 
+  // create accessor to access the private variable trackNo
   public int getTrack(){
     return trackNo;
   }
 
+  // create mutator to updata value name 
   public void setName(String n){
     this.name = n;
   }
 
+  // create mutator to updata value trackNo
   public void setTrackNo(int t){
     this.trackNo = t;
   }
