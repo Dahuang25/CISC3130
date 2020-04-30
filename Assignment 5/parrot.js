aa90ee1fac3d612c1ef2d0bfedd91d45757c5537
Dahuang25 <60986982+Dahuang25@users.noreply.github.com>
2020-04-30 19:39:32 -0400

    Add files via upload

---
+++ Assignment 5/parrot.js
@@ -0,0 +1,271 @@
+parrot={
+	src : "spritesheet.png",
+	img: undefined,
+	frame:0,
+	offset: 0,
+	"frames": [
+		{
+			"filename": "tile000.png",
+			"frame": {
+				"x": 1,
+				"y": 1,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile001.png",
+			"frame": {
+				"x": 253,
+				"y": 1,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile002.png",
+			"frame": {
+				"x": 1,
+				"y": 164,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile003.png",
+			"frame": {
+				"x": 253,
+				"y": 164,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile004.png",
+			"frame": {
+				"x": 1,
+				"y": 327,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile005.png",
+			"frame": {
+				"x": 253,
+				"y": 327,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile006.png",
+			"frame": {
+				"x": 1,
+				"y": 490,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile007.png",
+			"frame": {
+				"x": 253,
+				"y": 490,
+				"w": 250,
+				"h": 161
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 161
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 161
+			}
+		},
+		{
+			"filename": "tile008.png",
+			"frame": {
+				"x": 505,
+				"y": 1,
+				"w": 250,
+				"h": 160
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 160
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 160
+			}
+		},
+		{
+			"filename": "tile009.png",
+			"frame": {
+				"x": 505,
+				"y": 163,
+				"w": 250,
+				"h": 160
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 160
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 160
+			}
+		},
+		{
+			"filename": "tile010.png",
+			"frame": {
+				"x": 505,
+				"y": 325,
+				"w": 250,
+				"h": 160
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 160
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 160
+			}
+		},
+		{
+			"filename": "tile011.png",
+			"frame": {
+				"x": 505,
+				"y": 487,
+				"w": 250,
+				"h": 160
+			},
+			"rotated": false,
+			"trimmed": false,
+			"spriteSourceSize": {
+				"x": 0,
+				"y": 0,
+				"w": 250,
+				"h": 160
+			},
+			"sourceSize": {
+				"w": 250,
+				"h": 160
+			}
+		}
+	],
+	"meta": {
+		"app": "http://www.codeandweb.com/texturepacker",
+		"version": "1.0",
+		"image": "spritesheet.png",
+		"format": "RGBA8888",
+		"size": {
+			"w": 756,
+			"h": 652
+		},
+		"scale": "1"
+	}
+}
